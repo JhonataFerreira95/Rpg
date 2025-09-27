a8d66a01681313b71547dce14f7371ed98e49ad2
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-09-27 20:33:43 -0300

    adc o script player

---
+++ Assets/Scenes/script/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start é chamado ao inicializar o jogor, pode ser utilizado para setar alguma configuração incial
+    void Start()
+    {
+
+    }
+
+    // Update é chamado a cada frame, caso queira criar uma mecânica que seja execultada frame a frame, usamos o <update>
+    void Update()
+    {
+
+    }
+}
