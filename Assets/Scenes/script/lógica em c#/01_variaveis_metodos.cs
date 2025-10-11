24c275a23af6dcbc6b039d6cb79dfcbf011bdcf1
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-10-11 13:28:13 -0300

    trabalhando com variaveis

---
+++ Assets/Scenes/script/lógica em c#/01_variaveis_metodos.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variaveis : MonoBehaviour
+{
+    // Variável - Um objeto na memoria capaz de armazenar dados de diversos tipos
+
+    // Sintaxe no C# para escrever número <int>, <float>, <booleanas>, <string>
+
+    //Inteiro
+    int number = 10;
+
+    //Flutuante
+    float numberFloat = 10.2f;
+
+    //Booleanas
+    bool verdadeiro = true;
+    bool falso = false;
+
+    //String
+    string nome = "Bass";
+
+    // Variaveis próprias da unity
+
+    GameObject chao;
+    Transform inimigo;
+}
