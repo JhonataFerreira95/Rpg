f860d0a6033f2dd1c394e85497a32d5ed5b3a3c2
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-10-13 14:08:59 -0300

    trabalhando com switch

---
+++ Assets/Scenes/script/lógica em c#/03_switch.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// O switch funciona parecido com o if e else.
+public class Aula_switch : MonoBehaviour // É utilizado para evitar o uso exagerado de if/else
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
