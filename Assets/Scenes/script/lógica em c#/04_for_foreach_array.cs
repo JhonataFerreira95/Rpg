0030372edb32502959f09e130b8df4a0404ad016
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-10-16 21:16:59 -0300

    for, foreach e array

--- Assets/Scenes/script/lógica em c#/04_for_foreach_array.cs
+++ Assets/Scenes/script/lógica em c#/04_for_foreach_array.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class 04_for_foreach_array : MonoBehaviour
+public class For_foreach_array : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    // foreach e array
+   
+    public int[] arrayInt = { 1, 2, 3, 4, 5, 6, 7 };  // Sintaxe do array
+      
     void Start()
     {
+        // for (valorInicial; CondicionalFinal; ValorIncremento)
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log(i);
+        }
         
+        // Sintaxe foreach
+
+        foreach(int valor in arrayInt)
+        {
+            Debug.Log(valor);
+        }
     }
 
     // Update is called once per frame
