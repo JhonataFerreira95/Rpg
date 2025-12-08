68a580711b5baa0902ab8f1d8df5142865b9196f
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-08 01:51:36 -0300

    finalizado a animação de correr

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -19,23 +19,41 @@ public class PlayerAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(player.direction.sqrMagnitude > 0)
-        {
-            anim.SetInteger("transition", 1);
-        }
-        else
-        {
-            anim.SetInteger("transition", 0);
-        }
-
-        if(player.direction.x > 0)
-        {
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-
-        if(player.direction.x < 0)
-        {
-            transform.eulerAngles = new Vector2(0, 180);
-        }
+      OnMov(); 
+      OnRun();
     }
+
+    #region Movement
+
+        void OnMov()
+        {
+            if(player.direction.sqrMagnitude > 0)
+            {
+                anim.SetInteger("transition", 1);
+            }
+            else
+            {
+                anim.SetInteger("transition", 0);
+            }
+
+            if(player.direction.x > 0)
+            {
+                transform.eulerAngles = new Vector2(0, 0);
+            }
+
+            if(player.direction.x < 0)
+            {
+                transform.eulerAngles = new Vector2(0, 180);
+            }
+        }
+
+        void OnRun()
+        {
+        if (player.isRunning)
+        {
+            anim.SetInteger("transition", 2);
+        }
+        }
+
+    #endregion
 }
