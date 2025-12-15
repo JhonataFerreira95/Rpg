1cdf60eafdc2c996adeacaf74c40b40849e7a8a3
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-15 17:45:37 -0300

    criado variaveis de componentes

--- Assets/Scripts/Dialogue/DialogueControl.cs
+++ Assets/Scripts/Dialogue/DialogueControl.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueControl : MonoBehaviour
 {
+    [Header("Components")]
+    public GameObject dialogueObj;
+    public Image ProfileSprite;
+    public Text speechText;
+    public Text actorNameText;
+
     // Start is called before the first frame update
     void Start()
     {
