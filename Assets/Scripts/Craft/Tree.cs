d035f45925c88c9ee53d0c7fd5c8a5f3b4d4ad7f
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-23 00:34:05 -0300

    resolvido o bug dos drop da arvore dps de cortada

--- Assets/Scripts/Craft/Tree.cs
+++ Assets/Scripts/Craft/Tree.cs
@@ -11,6 +11,8 @@ public class Tree : MonoBehaviour
     [SerializeField] private int totalWood;
     [SerializeField] private ParticleSystem leafs;
 
+    private bool isCut;
+
     public void OnHit()
     {
         treeHealth--;
@@ -26,12 +28,14 @@ public class Tree : MonoBehaviour
                 Instantiate(woodPrefab, transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0f),  transform.rotation);
             }
             anim.SetTrigger("cut");
+
+            isCut = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      if(collision.CompareTag("Axe"))
+      if(collision.CompareTag("Axe") && !isCut)
         {
             OnHit();
         }  
