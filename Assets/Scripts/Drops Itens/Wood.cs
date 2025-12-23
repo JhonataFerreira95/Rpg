b3c56363fd11f5c968a091138b6ea05d04c5b8b2
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-22 23:09:49 -0300

    feito o drop da madeira

--- Assets/Scripts/Drops Itens/Wood.cs
+++ Assets/Scripts/Drops Itens/Wood.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Wood : MonoBehaviour
 {
 
-    [SerializeField] private float spped;
+    [SerializeField] private float speed;
     [SerializeField] private float timeMove;
 
     private float timeCount;
@@ -20,6 +21,11 @@ public class Wood : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeCount += Time.deltaTime;
+
+        if(timeCount < timeMove)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
     }
 }
