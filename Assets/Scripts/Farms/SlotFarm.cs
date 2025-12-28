8adc20d4c4a53f543bc456e3d41c2f9f0e7cac3b
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-28 16:02:45 -0300

    ajustando o colider e feito o script de cavar

--- Assets/Scripts/Farms/SlotFarm.cs
+++ Assets/Scripts/Farms/SlotFarm.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class SlotFarm : MonoBehaviour
 {
 
+    [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Sprite hole;
     [SerializeField] private Sprite carrot;
     [SerializeField] private int digAmount; // Quantidade de escavação
+
+    private int initialDigAmount;
+
+    private void Start()
+    {
+        initialDigAmount = digAmount;
+    }
 
     public void OnHit()
     {
         digAmount--;
 
+        if(digAmount <= initialDigAmount / 2)
+        {
+            spriteRenderer.sprite = hole;
+        }
+
         if(digAmount <= 0)
         {
             //plantar cenoura
-            
+            spriteRenderer.sprite = carrot;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      if(collision.CompareTag("Axe"))
+      if(collision.CompareTag("Dig"))
         {
             OnHit();
         }  
