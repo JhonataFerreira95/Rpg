2cba5378e3561b372e2dc8561f71a4e24eb2afad
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-28 16:41:01 -0300

    feito a logica para coletar água

--- Assets/Scripts/Farms/Water.cs
+++ Assets/Scripts/Farms/Water.cs
@@ -6,11 +6,14 @@ public class Water : MonoBehaviour
 {
 
     [SerializeField] private bool detectingPlayer;
+    [SerializeField] private int waterValue;
+
+    private PlayerItens player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerItens>();
     }
 
     // Update is called once per frame
@@ -18,7 +21,7 @@ public class Water : MonoBehaviour
     {
         if(detectingPlayer && Input.GetKeyDown(KeyCode.E))
         {
-            
+            player.WaterLimit(waterValue);
         }
     }
 
--- Assets/Scripts/PlayerItens.cs
+++ Assets/Scripts/PlayerItens.cs
@@ -6,5 +6,14 @@ public class PlayerItens : MonoBehaviour
 {
 
     public int totalWood;
-    public int totalWater;
+    public int currentWater;
+
+    private int waterLimit = 50;
+    public void WaterLimit(int Water)
+    {   
+       if(currentWater < waterLimit)
+        {
+          currentWater += Water;  
+        }
+    }
 }
