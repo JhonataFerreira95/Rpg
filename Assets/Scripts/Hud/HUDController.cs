cac149d3299139f5a220c8f0e366e7b742f137d0
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-02 03:38:32 -0300

    Feito os serialize do HUDController

--- Assets/Scripts/Hud/HUDController.cs
+++ Assets/Scripts/Hud/HUDController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour
 {
+
+    [SerializeField] private Image WaterUIBar;
+    [SerializeField] private Image WoodUIBar;
+    [SerializeField] private Image CarrotUIBar;
+
+
     // Start is called before the first frame update
     void Start()
     {
