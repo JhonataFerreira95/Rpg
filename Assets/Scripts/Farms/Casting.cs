35f2aab7cbe37c908a54f882290c3876ac22b0d0
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-03 19:36:09 -0300

    reaproveitando o script do water para o casting

--- Assets/Scripts/Farms/Casting.cs
+++ Assets/Scripts/Farms/Casting.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Casting : MonoBehaviour
 {
+    [SerializeField] private bool detectingPlayer;
+    [SerializeField] private int waterValue;
+
+    private PlayerItens player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerItens>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(detectingPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            player.WaterLimit(waterValue);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            detectingPlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            detectingPlayer = false;
+        }
     }
 }
