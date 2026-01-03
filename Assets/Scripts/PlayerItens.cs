d7666b4a55f16aad14f8e964d6af0b96a6d8b642
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-03 19:40:02 -0300

    adc ao playerItens os materiais provenientes da pesca

--- Assets/Scripts/PlayerItens.cs
+++ Assets/Scripts/PlayerItens.cs
@@ -9,11 +9,13 @@ public class PlayerItens : MonoBehaviour
     public int totalWood;
     public int carrots;
     public float currentWater;
+    public int fishes;
 
     [Header("Limits")]
     public float waterLimit = 50;
     public float woodLimit = 5;
     public float carrotLimit = 10;
+    public float fishesLimet = 5f;
 
     public void WaterLimit(float Water)
     {   
