b5b28a7b7b2e7f86777c8f2433b9fe4f19d741a1
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-05 14:49:50 -0300

    reaproveitando o script da water para seguir a mesma lógica no house

--- Assets/Scripts/Buldings/House.cs
+++ Assets/Scripts/Buldings/House.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
+    private bool detectingPlayer;
+    private PlayerItens player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerItens>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(detectingPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            detectingPlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            detectingPlayer = false;
+        }
     }
 }
