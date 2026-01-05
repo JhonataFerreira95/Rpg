0d754948f9ec9c6a92da061d11cf5a3f5ecfc721
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-05 15:26:09 -0300

    criado as variaveis de controles e feito a lógica do tempo para bulding

--- Assets/Scripts/Buldings/House.cs
+++ Assets/Scripts/Buldings/House.cs
@@ -6,8 +6,12 @@ public class House : MonoBehaviour
 {
     [SerializeField] private Color starColor;
     [SerializeField] private Color endColor;
+    [SerializeField] private float timeAmount;
+
     private bool detectingPlayer;
     private PlayerItens player;
+    private float timeCount;
+    private bool isBegining;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +24,16 @@ public class House : MonoBehaviour
     {
         if(detectingPlayer && Input.GetKeyDown(KeyCode.E))
         {
+            isBegining = true;
             
+        }
+        if(isBegining)
+        {
+            timeCount += Time.deltaTime;
+            if(timeCount >= timeAmount)
+            {
+                //casa é finalizda
+            }
         }
     }
 
