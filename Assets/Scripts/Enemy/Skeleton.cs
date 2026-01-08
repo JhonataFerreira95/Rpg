250df783ca17f80ac625f6233fdbcf6aaaa83e71
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-07 21:43:38 -0300

    começando a trabalhar com o NavMeshAgent no script do inimigo

--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Skeleton : MonoBehaviour
 {
+
+    [SerializeField] private float speed;
+    [SerializeField] private NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
